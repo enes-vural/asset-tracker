8757808bb07e4aad6994173d0e0598cba71e3417
Enes Vural <86451907+TrSaka@users.noreply.github.com>
2024-12-09 02:12:15 +0300

    feature: splash UI

---
+++ lib/presentation/view/splash/splash_view.dart
@@ -0,0 +1,35 @@
+import 'package:asset_tracker/core/routers/router.dart';
+import 'package:asset_tracker/presentation/view/widgets/circle_logo_widget.dart';
+import 'package:auto_route/annotations.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+
+    //Automatically pushes to splash screen after 3 second.
+    //The mounted avoids from lifecycle crash during wait.
+    Future.delayed(const Duration(seconds: 3)).then((_) => mounted
+        ? Routers.instance.pushReplaceNamed(context, Routers.loginPath)
+        : null);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // MediaQuery getter for responsive design.
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: CircleLogoWidget(radius: size.width / 4.0),
+    );
+  }
+}
