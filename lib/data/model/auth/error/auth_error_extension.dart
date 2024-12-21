94730791e970fcf2843b108188c54d6da8116cd4
Enes Vural <86451907+TrSaka@users.noreply.github.com>
2024-12-21 21:42:21 +0300

    refactor auth error modelize

---
+++ lib/data/model/auth/error/auth_error_extension.dart
@@ -0,0 +1,38 @@
+import 'package:asset_tracker/data/model/auth/error/auth_error_state.dart';
+import 'package:easy_localization/easy_localization.dart';
+
+import '../../../../core/config/localization/generated/locale_keys.g.dart';
+
+extension GetAuthErrorType on String {
+  ///error code a göre authErrorState enum u buluyor.
+  AuthErrorState searchErrorState(String errorCode) {
+    return AuthErrorState.values.firstWhere((state) => state.value == errorCode,
+        orElse: () => AuthErrorState.GENERAL_ERR);
+  }
+}
+
+extension ConvertAuthErrorType on AuthErrorState {
+  ///errorCode Enum Type ına göre içindeki değeri (Localize edilmiş hata mesajını alıyor)
+  String fromErrorToModel() {
+    switch (this) {
+      case AuthErrorState.ACCOUNT_EXIST:
+        return LocaleKeys.auth_response_accountExist.tr();
+      case AuthErrorState.INVALID_CRED:
+        return LocaleKeys.auth_response_invalidCred.tr();
+      case AuthErrorState.NOT_FOUND:
+        return LocaleKeys.auth_response_notFound.tr();
+      case AuthErrorState.INVALID_EMAIL:
+        return LocaleKeys.auth_response_invalidEmail.tr();
+      case AuthErrorState.REQUEST_QUOTA:
+        return LocaleKeys.auth_response_requestQuotaExceed.tr();
+      case AuthErrorState.TIMEOUT:
+        return LocaleKeys.auth_response_timeout.tr();
+      case AuthErrorState.USER_DISABLED:
+        return LocaleKeys.auth_response_userDisabled.tr();
+      case AuthErrorState.WRONG_PASSWORD:
+        return LocaleKeys.auth_response_wrongPassword.tr();
+      default:
+        return LocaleKeys.auth_response_generalErr.tr();
+    }
+  }
+}
--- lib/data/model/auth/error/auth_response_model.dart
+++ lib/data/model/auth/error/auth_response_model.dart
@@ -1,4 +1,5 @@
 import 'package:asset_tracker/core/config/localization/generated/locale_keys.g.dart';
+import 'package:asset_tracker/data/model/auth/error/auth_error_extension.dart';
 import 'package:asset_tracker/data/model/auth/error/auth_error_state.dart';
 import 'package:asset_tracker/data/model/base/error/error_model.dart';
 import 'package:easy_localization/easy_localization.dart';
@@ -11,40 +12,15 @@ final class AuthErrorModel extends BaseErrorModel {
   const AuthErrorModel({super.message, required this.errorCode});
 
   factory AuthErrorModel.toErrorModel(String errorCode) {
-    //gelen hata mesajından AuthErrorState i almaya çalışıyor
-    final AuthErrorState errorState = AuthErrorState.values.firstWhere(
-        (state) => state.value == errorCode,
-        orElse: () => AuthErrorState.GENERAL_ERR);
-    //errorState belirlendikten sonra localize edilmiş hata mesajını almka için _fromErrorToModel çağırılıyor.
+    //error code a göre authErrorState enum u buluyor.
+    final AuthErrorState errorState = errorCode.searchErrorState(errorCode);
 
-    //burada gelen hata kodundan error mesajımızı oluşturuyoruz.
-    String message = _fromErrorToModel(errorState);
+    //errorCode Enum Type ına göre içindeki değeri (Localize edilmiş hata mesajını alıyor)
+    String message = errorState.fromErrorToModel();
     //gelen hatayı mesaja çevirip nesneyi tekrar türetiyoruz
     return AuthErrorModel(errorCode: errorCode, message: message);
   }
   //static ama '_' ile erişilemiyor factory methodu uygulanabilsin diye ekledim
-  static String _fromErrorToModel(AuthErrorState? error) {
-    switch (error) {
-      case AuthErrorState.ACCOUNT_EXIST:
-        return LocaleKeys.auth_response_accountExist.tr();
-      case AuthErrorState.INVALID_CRED:
-        return LocaleKeys.auth_response_invalidCred.tr();
-      case AuthErrorState.NOT_FOUND:
-        return LocaleKeys.auth_response_notFound.tr();
-      case AuthErrorState.INVALID_EMAIL:
-        return LocaleKeys.auth_response_invalidEmail.tr();
-      case AuthErrorState.REQUEST_QUOTA:
-        return LocaleKeys.auth_response_requestQuotaExceed.tr();
-      case AuthErrorState.TIMEOUT:
-        return LocaleKeys.auth_response_timeout.tr();
-      case AuthErrorState.USER_DISABLED:
-        return LocaleKeys.auth_response_userDisabled.tr();
-      case AuthErrorState.WRONG_PASSWORD:
-        return LocaleKeys.auth_response_wrongPassword.tr();
-      default:
-        return LocaleKeys.auth_response_generalErr.tr();
-    }
-  }
 
   AuthErrorModel copyWith({
     String? message,
