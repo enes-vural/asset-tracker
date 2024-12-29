981af8ee1edd8b476553e39524559d1a2be1f294
Enes Vural <86451907+TrSaka@users.noreply.github.com>
2024-12-30 02:16:50 +0300

    feature: socket repository and viewmodel

--- lib/presentation/view_model/home/home_view_model.dart
+++ lib/presentation/view_model/home/home_view_model.dart
@@ -1,13 +1,20 @@
-import 'package:asset_tracker/data/repository/web/iweb_socket_repository.dart';
+import 'package:asset_tracker/domain/usecase/web/web_use_case.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewModel extends ChangeNotifier {
-  IWebSocketRepository service;
+  GetSocketStreamUseCase getSocketStreamUseCase;
 
-  HomeViewModel({required this.service});
+  HomeViewModel({required this.getSocketStreamUseCase});
 
   Future<void> getData() async {
-    await service.startConnection();
+    final data = await getSocketStreamUseCase.call(null);
+    print(data.toString());
+    data.fold((failure) => print(failure.message), (success) {
+      print("Connection STATE : " + success.state.toString());
+    }
+    );
   }
+
+  // we will get stream after ensure connection is done !
 }
 //test@gmail.com
