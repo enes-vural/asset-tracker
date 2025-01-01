1147b57ddf3e76c59a259973d4fb652b4e530d37
Enes Vural <86451907+TrSaka@users.noreply.github.com>
2025-01-01 05:38:29 +0300

    feature: currency stream has visualized in view

---
+++ lib/core/config/theme/extension/currency_widget_title_extension.dart
@@ -0,0 +1,110 @@
+import 'package:easy_localization/easy_localization.dart';
+
+import '../../localization/generated/locale_keys.g.dart';
+
+extension GetCurrencyTitle on String {
+  String getCurrencyTitle() {
+    switch (this) {
+      case "ata5_yeni":
+        return LocaleKeys.home_widget_ata5_yeni.tr();
+      case "ata5_eski":
+        return LocaleKeys.home_widget_ata5_eski.tr();
+      case "gremese_yeni":
+        return LocaleKeys.home_widget_gremese_yeni.tr();
+      case "gremese_eski":
+        return LocaleKeys.home_widget_gremese_eski.tr();
+      case "ayar14":
+        return LocaleKeys.home_widget_ayar14.tr();
+      case "gumustry":
+        return LocaleKeys.home_widget_gumustry.tr();
+      case "gumususd":
+        return LocaleKeys.home_widget_gumususd.tr();
+      case "audusd":
+        return LocaleKeys.home_widget_audusd.tr();
+      case "gbpusd":
+        return LocaleKeys.home_widget_gbpusd.tr();
+      case "platin":
+        return LocaleKeys.home_widget_platin.tr();
+      case "paladyum":
+        return LocaleKeys.home_widget_paladyum.tr();
+      case "usdjpy":
+        return LocaleKeys.home_widget_usdjpy.tr();
+      case "usdchf":
+        return LocaleKeys.home_widget_usdchf.tr();
+      case "usdcad":
+        return LocaleKeys.home_widget_usdcad.tr();
+      case "eurusd":
+        return LocaleKeys.home_widget_eurusd.tr();
+      case "usdsar":
+        return LocaleKeys.home_widget_usdsar.tr();
+      case "noktry":
+        return LocaleKeys.home_widget_noktry.tr();
+      case "audtry":
+        return LocaleKeys.home_widget_audtry.tr();
+      case "altin":
+        return LocaleKeys.home_widget_altin.tr();
+      case "usdpure":
+        return LocaleKeys.home_widget_usdpure.tr();
+      case "usdtry":
+        return LocaleKeys.home_widget_usdtry.tr();
+      case "ons":
+        return LocaleKeys.home_widget_ons.tr();
+      case "eurtry":
+        return LocaleKeys.home_widget_eurtry.tr();
+      case "usdkg":
+        return LocaleKeys.home_widget_usdkg.tr();
+      case "eurkg":
+        return LocaleKeys.home_widget_eurkg.tr();
+      case "ayar22":
+        return LocaleKeys.home_widget_ayar22.tr();
+      case "gbptry":
+        return LocaleKeys.home_widget_gbptry.tr();
+      case "chftry":
+        return LocaleKeys.home_widget_chftry.tr();
+      case "kulcealtin":
+        return LocaleKeys.home_widget_kulcealtin.tr();
+      case "xauxag":
+        return LocaleKeys.home_widget_xauxag.tr();
+      case "ceyrek_yeni":
+        return LocaleKeys.home_widget_ceyrek_yeni.tr();
+      case "cadtry":
+        return LocaleKeys.home_widget_cadtry.tr();
+      case "ceyrek_eski":
+        return LocaleKeys.home_widget_ceyrek_eski.tr();
+      case "sartry":
+        return LocaleKeys.home_widget_sartry.tr();
+      case "yarim_yeni":
+        return LocaleKeys.home_widget_yarim_yeni.tr();
+      case "yarim_eski":
+        return LocaleKeys.home_widget_yarim_eski.tr();
+      case "jpytry":
+        return LocaleKeys.home_widget_jpytry.tr();
+      case "tek_yeni":
+        return LocaleKeys.home_widget_tek_yeni.tr();
+      case "tek_eski":
+        return LocaleKeys.home_widget_tek_eski.tr();
+      case "ata_yeni":
+        return LocaleKeys.home_widget_ata_yeni.tr();
+      case "dkktry":
+        return LocaleKeys.home_widget_dkktry.tr();
+      case "xauusd":
+        return LocaleKeys.home_widget_xauusd.tr();
+      case "xagusd":
+        return LocaleKeys.home_widget_xagusd.tr();
+      case "xptusd":
+        return LocaleKeys.home_widget_xptusd.tr();
+      case "xpdusd":
+        return LocaleKeys.home_widget_xpdusd.tr();
+      case "plntry":
+        return LocaleKeys.home_widget_plntry.tr();
+      case "huftry":
+        return LocaleKeys.home_widget_huftry.tr();
+      case "sektry":
+        return LocaleKeys.home_widget_sektry.tr();
+      case "zartry":
+        return LocaleKeys.home_widget_zartry.tr();
+      default:
+        return "Unknown Currency";
+    }
+  }
+}
--- lib/presentation/view/home/home_view.dart
+++ lib/presentation/view/home/home_view.dart
@@ -1,7 +1,16 @@
+import 'package:asset_tracker/core/config/localization/generated/locale_keys.g.dart';
+import 'package:asset_tracker/core/config/theme/extension/responsive_extension.dart';
+import 'package:asset_tracker/core/widgets/custom_padding.dart';
+import 'package:asset_tracker/domain/entities/web/socket/currency_entity.dart';
+import 'package:asset_tracker/domain/entities/web/socket/currency_widget_entity.dart';
 import 'package:asset_tracker/injection.dart';
+import 'package:asset_tracker/presentation/view_model/home/home_view_model.dart';
 import 'package:auto_route/annotations.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../widgets/currency_card_widget.dart';
 
 @RoutePage()
 class HomeView extends ConsumerStatefulWidget {
@@ -12,19 +21,88 @@ class HomeView extends ConsumerStatefulWidget {
 }
 
 class _HomeViewState extends ConsumerState<HomeView> {
+  //async function to call data
+  void callData() async => await ref.read(homeViewModelProvider).getData();
+
+  @override
+  void initState() {
+    //initialize all streams when page starts
+    callData();
+    super.initState();
+  }
+
+  //TODO: Dispose method need for discard stream after navigate to another page
+
   @override
   Widget build(BuildContext context) {
+    final HomeViewModel viewModel = ref.watch(homeViewModelProvider);
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(title: Text(LocaleKeys.app_title.tr())),
       body: Center(
         child: TextButton(
-          onPressed: () async {
-            await ref.read(homeViewModelProvider).getData();
-          },
-          child: Text("Click on Me"),
+          onPressed: () {},
+          child: CustomPadding.largeHorizontal(
+            widget: SizedBox(
+              height: ResponsiveSize(context).screenHeight.toPercent(75),
+              width: ResponsiveSize(context).screenWidth,
+              child: StreamBuilder(
+                stream: viewModel.getStream(),
+                builder: (context, snapshot) {
+                  //show circular progess bar while waiting for data
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+
+                  if (snapshot.connectionState == ConnectionState.active) {
+                    // If stream is active and data is available
+                    if (snapshot.hasData) {
+                      List<CurrencyEntity> data = snapshot.data;
+
+                      return ListView.builder(
+                        itemCount: data.length, // Adjust based on data
+                        itemBuilder: (context, index) {
+                          CurrencyWidgetEntity currency =
+                              CurrencyWidgetEntity.fromCurrency(data[index]);
+                          return CurrencyCardWidget(currency: currency);
+                        },
+                      );
+                    } else {
+                      // No data available yet, but stream is active
+                      return loadingTextWidget();
+                    }
+                  }
+                  //default state
+                  return loadingTextWidget();
+                },
+              ),
+            ),
+          ),
         ),
       ),
     );
   }
+
+  Center loadingTextWidget() => Center(child: Text(LocaleKeys.home_wait.tr()));
 }
-//test@gmail.com
+
+
+/*
+TODO: We will inspect here before remove so it should be stay here :)
+-------------------
+  if (snapshot.connectionState == ConnectionState.done) {
+    // If the stream is done and no data was received
+    if (snapshot.hasData) {
+      var data = snapshot.data;
+      return ListView.builder(
+        itemCount: data.length,
+        itemBuilder: (context, index) {
+          var item = data[index];
+          return Text("Currency: ${item}");
+        },
+      );
+    } else {
+      return Center(child: Text('No Data Available'));
+    }
+  }
+-------------------
+*/
