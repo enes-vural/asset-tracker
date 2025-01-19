969924aced1753e820776af710948d7742f81290
Enes Vural <86451907+TrSaka@users.noreply.github.com>
2025-01-19 03:49:54 +0300

    auth test

---
+++ test/repository/auth/auth_repository_test.dart
@@ -0,0 +1,57 @@
+import 'package:asset_tracker/data/repository/auth/auth_repository.dart';
+import 'package:asset_tracker/data/service/remote/auth/auth_service.dart';
+import 'package:asset_tracker/data/service/remote/auth/iauth_service.dart';
+import 'package:asset_tracker/domain/entities/auth/error/auth_error_entity.dart';
+import 'package:asset_tracker/domain/entities/auth/user_login_entity.dart';
+import 'package:asset_tracker/domain/entities/auth/user_login_response_entity.dart';
+import 'package:asset_tracker/domain/repository/auth/iauth_repository.dart';
+import 'package:dartz/dartz.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+import '../../shared/auth_service_shared.dart';
+
+void main() {
+  late MockAuthHelper mockAuthHelper;
+  late IAuthService firebaseAuthService;
+  late IAuthRepository authRepo;
+
+  setUp(() {
+    mockAuthHelper = MockAuthHelper();
+    firebaseAuthService =
+        FirebaseAuthService(authService: mockAuthHelper.mockFirebaseAuth);
+
+    authRepo = FirebaseAuthRepository(authService: firebaseAuthService);
+  });
+
+  group("Authentication Repository (Data)", () {
+    const UserLoginEntity successLoginEntity =
+        UserLoginEntity(userName: "test@gmail.com", password: "123456");
+
+    test("Authentication Repository Success Test", () async {
+      mockAuthHelper.whenSuccessLogin(
+          successLoginEntity.userName, successLoginEntity.password);
+
+      when(mockAuthHelper.userCreds.user).thenReturn(mockAuthHelper.user);
+      when(mockAuthHelper.user.uid).thenReturn('success-credential');
+      when(mockAuthHelper.user.getIdToken())
+          .thenAnswer((_) async => "success-token");
+
+      final signInResult = await authRepo.signIn(successLoginEntity);
+
+      expect(signInResult, isA<Right<void, UserLoginResponseEntity>>());
+      signInResult.fold(
+          (l) => null, (right) => expect(right.token, "success-token"));
+    });
+
+    test("Authentication Repository Failed (User not found) Test", () async {
+      mockAuthHelper.whenFailedLogin(
+          successLoginEntity.userName, successLoginEntity.password);
+      when(mockAuthHelper.userCreds.user?.uid).thenReturn(null);
+
+      final signInResult = await authRepo.signIn(successLoginEntity);
+
+      expect(signInResult, isA<Left<AuthErrorEntity, void>>());
+    });
+  });
+}
