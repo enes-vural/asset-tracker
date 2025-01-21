f2fa5801cbc2a7199e38841cfd35f9249d7b4946
Enes Vural <86451907+TrSaka@users.noreply.github.com>
2025-01-21 03:59:47 +0300

    login view ui test

--- integration_test/helper/integration_test_helper.dart
+++ integration_test/helper/integration_test_helper.dart
@@ -1,10 +1,26 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:asset_tracker/main.dart' as app;
+import 'package:integration_test/integration_test.dart';
 
 final class IntegrationTestHelper {
   IntegrationTestHelper._();
 
-  static Future<void> waitFrame(WidgetTester tester, int? seconds) async =>
-      await tester.pumpAndSettle(Duration(seconds: seconds ?? 0));
+  static void initailize() {
+    final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+    binding.framePolicy = LiveTestWidgetsFlutterBindingFramePolicy.fullyLive;
+    WidgetsFlutterBinding.ensureInitialized();
+  }
+
+  static void startApp() => app.main();
+
+  static Future<void> waitFrame(WidgetTester tester, [int? seconds]) async {
+    if (seconds == null || seconds <= 0) {
+      await tester.pumpAndSettle();
+    } else {
+      await tester.pumpAndSettle(Duration(seconds: seconds));
+    }
+  }
 
   static Future<void> pumpFrame(WidgetTester tester) async =>
       await tester.pump();
