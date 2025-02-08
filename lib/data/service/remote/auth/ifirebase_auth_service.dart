964be22d0e67e5731c6862156c4d697ac123f067
Enes Vural <86451907+TrSaka@users.noreply.github.com>
2025-02-08 13:33:33 +0300

    small type changes

--- lib/data/service/remote/auth/ifirebase_auth_service.dart
+++ lib/data/service/remote/auth/ifirebase_auth_service.dart
@@ -1,8 +1,5 @@
 import 'package:asset_tracker/data/model/auth/firebase_auth_user_model.dart';
 import 'package:asset_tracker/data/service/remote/auth/iauth_service.dart';
-import '../../../../domain/entities/auth/user_login_entity.dart';
-
-abstract interface class IFirebaseAuthService implements IAuthService {
-  @override
-  Future<FirebaseAuthUser>? signInUser(UserLoginEntity entity);
+abstract interface class IFirebaseAuthService
+    implements IAuthService<FirebaseAuthUser> {
 }
